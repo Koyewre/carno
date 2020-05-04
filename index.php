4382375a78758529c7985e2fcbf9603df99fed8b
Koyewre <yr4uk05072005@gmail.com>
2020-05-04 21:43:38 +0300

    21:43 04.05.2020

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+if ($_POST['text']=='qwerty'){
+    echo('Вхыд здійснено   ');
+}
+else{
+    echo ('Ты че дебил   ');
+}
+
+newClass Human{
+    private $name;
+    private $old;
+
+    function Set_year()
+
+}
+
+
+
+?>
